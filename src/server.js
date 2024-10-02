ef96fd8d41248d9475e324faea904afe79a5cebd
Marlus Weber Midginski <marluswm7@gmail.com>
2024-10-01 22:31:21 -0300

    SQL Database

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ require('express-async-errors');
 const express = require("express");
 const routes = require("./routes/index.js");
 const AppError = require("./utils/AppError.js");
+const database = require('./database/sqlite');
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+database();
 app.use((error, request, response, next)=> {
     if(error instanceof AppError){
         return response.status(error.statusCode).json({
