5c7ca085faeb04703e060de36e95f8fe8e6c1e6d
Marlus Weber Midginski <marluswm7@gmail.com>
2024-10-12 18:00:20 -0300

    Migrations DB Knex. Auto table create

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,16 @@
-const { path } = require("express/lib/application");
+
+const path = require('path')
 
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, 'src', 'database', database.db)
+      filename: path.resolve(__dirname, "src", "database", "database.db")
     },
-    migration: {
-      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
+    migrations:{
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
-
     useNullAsDefault: true
   },
 };
