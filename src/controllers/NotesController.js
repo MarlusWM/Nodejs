896b661381e75c369b7f16433874f895b2ba3ad1
“Marlus” <marluswm7@gmail.com>
2024-10-15 22:41:53 -0300

    Where, WhereIn and create show, delete and index functions

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express');
 const knex = require('../database/knex')
 
 class NotesController{
@@ -32,6 +33,43 @@ class NotesController{
         await knex('tags').insert(tagsInsert);
 
         response.json();
+    }
+
+    async show(request, response){
+        const { id } = request.params;
+
+        const note = await knex("notes").where({ id }).first();
+        const tags = await knex("tags").where({ note_id: id }).orderBy("name")
+        const links = await knex("links").where({ note_id: id }).orderBy("created_at")
+
+        return response.json({
+            ...note,
+            tags,
+            links
+        });
+    }
+
+    async delete(request, response){
+        const { id } = request.params;
+
+        await knex("notes").where({ id }).delete();
+
+        return response.json();
+    }
+
+    async index(request, response){
+        const { user_id } = require.body;
+
+        let notes;
+        if (tags) {
+            const filterTags = tags.split(',').map(tag => tag.trim());
+
+            notes = await knex("tags").whereIn("name", filterTags);
+        }else{
+            notes = await knex("notes").where({ user_id }).whereLike( "title", `%${title}%`).orderBy("title");
+        }
+
+        return response.json();
     }
 }
 
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -9,6 +9,9 @@ function myMiddleware(request, response, next){
 
 const notesController = new NotesController();
 
+notesRoutes.get('/', notesController.index);
 notesRoutes.post('/:user_id', notesController.create);
+notesRoutes.get('/:id', notesController.show);
+notesRoutes.delete('/:id', notesController.delete);
 
 module.exports = notesRoutes;
